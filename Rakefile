8a31c5359459bebf89dc2db060febd66be4976c3
pjkelly <me@pjkel.ly>
2011-04-26 11:34:33 -0700

    Added versions task.

--- Rakefile
+++ Rakefile
@@ -10,5 +10,5 @@ begin
     gemspec.add_dependency "tinder", ">= 1.4.0"
   end
 rescue LoadError
-  puts "Jeweler not available. Install it with: sudo gem install technicalpickles-jeweler -s http://gems.github.com"
+  puts "Jeweler not available. Install it with: gem install jeweler"
 end
--- lib/crushserver/recipes.rb
+++ lib/crushserver/recipes.rb
@@ -66,37 +66,50 @@ Capistrano::Configuration.instance(:must_exist).load do
       end
     end
 
-    # desc "Send a message to the campfire chat room about the deploy start"
-    # task :snitch_begin do
-    #   set :snitch_message, "BEGIN DEPLOY [#{stage.to_s.upcase}]: #{ENV['USER']}, #{branch}/#{real_revision[0, 7]} to #{deploy_to}"
-    #   snitch
-    # end
-
     desc "Send a message to the campfire chat room about the deploy end"
     task :snitch_end do
       # set :snitch_message, "END DEPLOY [#{stage.to_s.upcase}]: #{ENV['USER']}, #{branch}/#{real_revision[0, 7]} to #{deploy_to}"
       set :snitch_message, "#{application.upcase} was deployed to #{stage.to_s.upcase} by #{ENV['USER'].upcase} (#{branch}/#{real_revision[0, 7]})"
       snitch
     end
+  end
 
-    # desc "Send a message to the campfire chat roob about the rollback"
-    # task :snitch_rollback do
-    #   set :snitch_message, "ROLLBACK [#{stage.to_s.upcase}]: #{ENV['USER']}, #{latest_revision[0, 7]} to #{previous_revision[0, 7]} on #{deploy_to}"
-    #   snitch
-    # end
+  desc "Show currently deployed revision on server."
+  task :revisions, :roles => :app do
+    begin
+      current, previous, latest = current_revision[0,7], previous_revision[0,7], real_revision[0,7]
+      puts "\n" << "-"*63
+      puts "===== Master Revision: \033[1;33m#{latest}\033[0m\n\n"
+      puts "===== [ \033[1;36m#{application} - #{stage}\033[0m ]"
+      puts "=== Deployed Revision: \033[1;32m#{current}\033[0m"
+      puts "=== Previous Revision: \033[1;32m#{previous}\033[0m\n\n"
+
+      # If deployed and master are the same, show the difference between the last 2 deployments.
+      base_label, new_label, base_rev, new_rev = latest != current ? \
+           ["deployed", "master", current, latest] : \
+           ["previous", "deployed", previous, current]
+
+      # Show difference between master and deployed revisions.
+      if (diff = `git log #{base_rev}..#{new_rev} --oneline`) != ""
+        # Colorize refs
+        diff.gsub!(/^([a-f0-9]+) /, "\033[1;32m\\1\033[0m - ")
+        diff = "    " << diff.gsub("\n", "\n    ") << "\n"
+        # Indent commit messages nicely, max 80 chars per line, line has to end with space.
+        diff = diff.split("\n").map{|l|l.scan(/.{1,120}/).join("\n"<<" "*14).gsub(/([^ ]*)\n {14}/m,"\n"<<" "*14<<"\\1")}.join("\n")
+        puts "=== Difference between #{base_label} revision and #{new_label} revision:\n\n"
+        puts diff
+      end
+    rescue
+        puts "=== Revisions Task Failed!"
+    end
   end
 
   #############################################################
   # Hooks
   #############################################################
 
-  # before :deploy do
-  #   campfire.snitch_begin unless ENV['QUIET'].to_i > 0
-  # end
-
   after :deploy do
     campfire.snitch_end unless ENV['QUIET'].to_i > 0
+    revisions
   end
-
-  # before 'deploy:rollback', 'campfire:snitch_rollback'
 end
