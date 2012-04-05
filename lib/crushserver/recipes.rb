b902b4e9215789b38de1e60d85a2846b767ae40b
PJ Kelly <me@pjkel.ly>
2012-04-05 11:26:42 -0700

    Switched over from Tinder/Campfire notifications to Hipchat.

--- lib/crushserver/recipes.rb
+++ lib/crushserver/recipes.rb
@@ -1,3 +1,6 @@
+require 'hipchat/capistrano'
+require 'yaml'
+
 Capistrano::Configuration.instance(:must_exist).load do
   namespace(:db) do
     desc "Execute db:seed rake task in appropriate environment"
@@ -25,77 +28,11 @@ Capistrano::Configuration.instance(:must_exist).load do
     end
   end
 
-  namespace :campfire do
-    desc "Send a message to the campfire chat room"
-    task :snitch do
-      if ENV['CAMPFIRE_SUBDOMAIN'].blank? || ENV['CAMPFIRE_TOKEN'].blank? || ENV['CAMPFIRE_ROOM'].blank?
-        puts "Campfire notifications are not configured in your environment. The CAMPFIRE_SUBDOMAIN, CAMPFIRE_TOKEN and CAMPFIRE_ROOM environment variables must be set."
-      else
-        campfire = Tinder::Campfire.new ENV['CAMPFIRE_SUBDOMAIN'], :ssl => true, :token => ENV['CAMPFIRE_TOKEN']
-        room = campfire.find_room_by_name ENV['CAMPFIRE_ROOM']
-        snitch_message = fetch(:snitch_message) { ENV['MESSAGE'] || abort('Capfire snitch message is missing. Use set :snitch_message, "Your message"') }
-        room.paste(snitch_message)
-      end
-    end
-
-    desc "Send a message to the campfire chat room about the deploy start"
-    task :snitch_start do
-      message = "#{ENV['USER'].upcase} is deploying #{application.upcase} to #{stage.to_s.upcase}.  Please stand by..."
-      set :snitch_message, message
-      snitch
-    end
-
-    desc "Send a message to the campfire chat room about the deploy end"
-    task :snitch_end do
-      revisions
-      message = <<HERE
-#{application.upcase} was deployed to #{stage.to_s.upcase} by #{ENV['USER'].upcase} (#{branch}/#{real_revision[0, 7]})
-
-#{revisions_result}
-HERE
-      set :snitch_message, message
-      snitch
-    end
-  end
-
-  desc "Show currently deployed revision on server."
-  task :revisions, :roles => :app do
-    result = String.new
-    begin
-      current, previous, latest = current_revision[0,7], previous_revision[0,7], real_revision[0,7]
-      result << "===== Master Revision: #{latest}\n\n"
-      result << "===== [ #{application} - #{stage} ]\n"
-      result << "=== Deployed Revision: #{current}\n"
-      result << "=== Previous Revision: #{previous}\n"
-
-      # If deployed and master are the same, show the difference between the last 2 deployments.
-      base_label, new_label, base_rev, new_rev = latest != current ? \
-           ["deployed", "master", current, latest] : \
-           ["previous", "deployed", previous, current]
-
-      # Show difference between master and deployed revisions.
-      if (diff = `git log #{base_rev}..#{new_rev} --oneline`) != ""
-        diff = "    " << diff.gsub("\n", "\n    ") << "\n"
-        # Indent commit messages nicely, max 80 chars per line, line has to end with space.
-        diff = diff.split("\n").map{|l|l.scan(/.{1,120}/).join("\n"<<" "*14).gsub(/([^ ]*)\n {14}/m,"\n"<<" "*14<<"\\1")}.join("\n")
-        result << "=== Difference between #{base_label} revision and #{new_label} revision:\n\n"
-        result << diff
-      end
-    rescue
-      result << "=== Revisions Task Failed!"
-    end
-    set :revisions_result, result
-  end
-
-  #############################################################
-  # Hooks
-  #############################################################
-
-  before :deploy do
-    campfire.snitch_start unless ENV['QUIET'].to_i > 0
-  end
-
-  after :deploy do
-    campfire.snitch_end unless ENV['QUIET'].to_i > 0
+  crushserver_config_file = File.join(ENV['HOME'], '.crushserver.yml')
+  if File.exist?(crushserver_config_file)
+    crushserver_config = YAML.load_file(crushserver_config_file)
+    set :hipchat_token, crushserver_config['hipchat']['token']
+    set :hipchat_room_name, crushserver_config['hipchat']['room_name']
+    set :hipchat_announce, crushserver_config['hipchat']['announce']
   end
 end
